69f4c38f1e411f57e4d2efd958d1782305f001e4
franksun <sun_si_yan@163.com>
2019-03-15 15:23:38 +0800

    用户维护页面布局

--- src/page/pages/userMaintenance.vue
+++ src/page/pages/userMaintenance.vue
@@ -37,26 +37,26 @@
     </el-form>
     <!--全局操作-->
     <div class="marBottom20">
-      <el-button>新增</el-button>
-      <el-button>启用</el-button>
-      <el-button>禁用</el-button>
-      <el-button>删除</el-button>
+      <el-button @click="dialogVisible = true">新增</el-button>
+      <el-button @click="enableData">启用</el-button>
+      <el-button @click="disableData">禁用</el-button>
+      <el-button @click="deleteData">删除</el-button>
     </div>
     <el-table :data="tableData" border>
       <el-table-column type="selection" fixed />
-      <el-table-column type="index" label="序号" width="50" header-align="center" align="center" />
-      <el-table-column label="用户名" header-align="center" align="center" />
-      <el-table-column label="登录账号" header-align="center" align="center" />
-      <el-table-column label="所属组织" header-align="center" align="center" />
-      <el-table-column label="用户角色" header-align="center" align="center" />
-      <el-table-column label="手机" header-align="center" align="center" />
-      <el-table-column label="邮箱" header-align="center" align="center" />
-      <el-table-column label="状态" header-align="center" align="center" />
+      <el-table-column type="index" label="序号" width="60" header-align="center" align="center" />
+      <el-table-column label="用户名" width="100" header-align="center" align="center" />
+      <el-table-column label="登录账号" width="100" header-align="center" align="center" />
+      <el-table-column label="所属组织" width="100" header-align="center" align="center" />
+      <el-table-column label="用户角色" width="100" header-align="center" align="center" />
+      <el-table-column label="手机" width="100" header-align="center" align="center" />
+      <el-table-column label="邮箱" width="100" header-align="center" align="center" />
+      <el-table-column label="状态" width="100" header-align="center" align="center" />
       <el-table-column label="创建人" width="200" header-align="center" align="center" />
       <el-table-column label="创建时间" width="200" header-align="center" align="center" />
       <el-table-column label="操作" width="120" header-align="center" align="center" fixed="right">
         <template slot-scope="scope">
-          <el-button type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small" @click="dialogVisible = true">编辑</el-button>
           <el-button type="text" size="small">重置密码</el-button>
         </template>
       </el-table-column>
@@ -69,6 +69,57 @@
       :firstPage='options.firstPage'
       :lastPage='options.lastPage'
       @handleCurrentChangeSub="handleCurrentChange" />
+    <!--新增编辑-->
+    <el-dialog
+      title="新增/编辑管理用户信息"
+      :visible.sync="dialogVisible"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false">
+      <el-form label-width="150px">
+        <el-form-item label="所属组织：">
+          <el-select v-model="organization">
+            <el-option
+              v-for="item in organizationList"
+              :label="item.label"
+              :value="item.value"
+              :key="item.value" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="用户名：">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="登录账号：">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="登录密码：">
+          <el-input type="password" />
+        </el-form-item>
+        <el-form-item label="手机号码：">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="电子邮箱：">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="启用状态：">
+          <el-radio-group v-model="status">
+            <el-radio :label="0">启用</el-radio>
+            <el-radio :label="1">禁用</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="分配角色：">
+          <el-checkbox-group v-model="checkListValue">
+            <template v-for="(item,index) in checkList">
+              <el-checkbox :label="item.value">{{item.label}}</el-checkbox>
+              <br>
+            </template>
+          </el-checkbox-group>
+        </el-form-item>
+      </el-form>
+      <span slot="footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button @click="dialogVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
   </Box>
 </template>
 
@@ -79,6 +130,7 @@
     components:{Box},
     data(){
       return {
+        dialogVisible:false,
         organizationList:[],
         tableData:[
           {},{}
@@ -88,6 +140,11 @@
           {label:'禁用',value:0},
         ],
         organization:'',
+        checkListValue:[],
+        checkList:[
+          {label:'系统管理员', value:1},
+          {label:'区域经理',value:2}
+        ],
         username:'',
         status:'',
         options:{
@@ -103,6 +160,42 @@
       // 改变当前页码
       handleCurrentChange(val){
         console.log(val)
+      },
+      // 启用
+      enableData() {
+        this.$confirm('请问是否确认启用当前的记录?','确认提示',{
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(()=>{
+
+        }).catch(()=>{
+          return;
+        });
+      },
+      // 禁用
+      disableData(){
+        this.$confirm('请问是否确认禁用当前的记录?','确认提示',{
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(()=>{
+
+        }).catch(()=>{
+          return;
+        });
+      },
+      // 删除
+      deleteData(){
+        this.$confirm('请问是否确认删除当前的记录?','确认提示',{
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(()=>{
+
+        }).catch(()=>{
+          return;
+        });
       }
     },
     created(){
