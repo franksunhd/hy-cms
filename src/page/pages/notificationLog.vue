71291986b5150afddd86412610666eb668e68924
franksunhd <sun_si_yan@163.com>
2019-03-20 17:29:44 +0800

    通知日志面包屑

--- src/page/pages/notificationLog.vue
+++ src/page/pages/notificationLog.vue
@@ -1,10 +1,21 @@
 <template>
-    
+  <Box>
+    <!--面包屑区域-->
+    <div class="Breadcrumb">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item>{{$t('breadcrumb.systemSetting')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.logManagement')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.notificationLog')}}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </Box>
 </template>
 
 <script>
+  import Box from '../../components/Box';
   export default {
     name: "notificationLog",
+    components:{Box},
     data() {
       return {}
     },
