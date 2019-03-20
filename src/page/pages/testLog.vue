28fb20b8098b0d170e2f494d6072967db43335a5
franksunhd <sun_si_yan@163.com>
2019-03-20 17:31:20 +0800

    检测日志面包屑

--- src/page/pages/testLog.vue
+++ src/page/pages/testLog.vue
@@ -1,10 +1,21 @@
 <template>
-    
+  <Box>
+    <!--面包屑区域-->
+    <div class="Breadcrumb">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item>{{$t('breadcrumb.systemSetting')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.logManagement')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.testLog')}}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </Box>
 </template>
 
 <script>
+  import Box from '../../components/Box';
   export default {
     name: "testLog",
+    components:{Box},
     data() {
       return {}
     },
