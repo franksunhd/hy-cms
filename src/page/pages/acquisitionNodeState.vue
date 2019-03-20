19e823e3f47be6f975ffcaaa950f29bfa3549d1a
franksunhd <sun_si_yan@163.com>
2019-03-20 16:09:49 +0800

    采集节点状态

--- src/page/pages/acquisitionNodeState.vue
+++ src/page/pages/acquisitionNodeState.vue
@@ -1,12 +1,25 @@
 <template>
-    
+  <Box>
+    <!--面包屑区域-->
+    <div class="Breadcrumb">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item>{{$t('breadcrumb.systemSetting')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.deviceAcquisitionSettings')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.acquisitionNodeState')}}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </Box>
 </template>
 
 <script>
+  import Box from '../../components/Box';
   export default {
     name: "acquisitionNodeState",
+    components:{Box},
     data() {
-      return {}
+      return {
+
+      }
     },
     methods: {},
     created() {
