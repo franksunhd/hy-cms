ae88ed4b6c0073efadcf928b2adf0dc132be24f1
franksunhd <sun_si_yan@163.com>
2019-03-25 10:34:42 +0800

    修改采集节点状态页面样式

--- src/page/pages/acquisitionNodeState.vue
+++ src/page/pages/acquisitionNodeState.vue
@@ -8,73 +8,94 @@
         <el-breadcrumb-item>{{$t('breadcrumb.acquisitionNodeState')}}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <!--表单-->
-    <el-form inline>
-      <el-form-item :label="$t('acquisitionNodeState.nodeName') + '：'">
-        <el-input />
-      </el-form-item>
-      <el-form-item :label="$t('acquisitionNodeState.nodeIp') + '：'">
-        <el-input />
-      </el-form-item>
-      <el-form-item :label="$t('acquisitionNodeState.nodePort') + '：'">
-        <el-input />
-      </el-form-item>
-      <br>
-      <el-form-item :label="$t('acquisitionNodeState.groupName') + '：'">
-        <el-select>
-          <el-option></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item :label="$t('acquisitionNodeState.nodeType') + '：'">
-        <el-select>
-          <el-option></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item :label="$t('acquisitionNodeState.status') + '：'">
-        <el-select>
-          <el-option></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">{{$t('public.query')}}</el-button>
-      </el-form-item>
-    </el-form>
-    <!--全局操作-->
-    <div class="marBottom20 clearfix">
-      <el-button @click="runMonitoring">{{$t('acquisitionNodeState.runMonitoring')}}</el-button>
-      <el-button @click="stopMonitoring">{{$t('acquisitionNodeState.stopMonitoring')}}</el-button>
-
-      <el-button @click="banMonitoring">{{$t('acquisitionNodeState.banMonitoring')}}</el-button>
-      <el-button @click="temporarilyIgnore">{{$t('acquisitionNodeState.temporarilyIgnore')}}</el-button>
-      <el-button @click="cancelIgnore">{{$t('acquisitionNodeState.cancelIgnore')}}</el-button>
-      <el-button @click="offLine">{{$t('acquisitionNodeState.offLine')}}</el-button>
-      <el-select class="fr">
-        <el-option></el-option>
-      </el-select>
+    <div class="padding20 borderBottom">
+      <!--表单-->
+      <el-form inline>
+        <el-form-item :label="$t('acquisitionNodeState.nodeName') + '：'" style="margin-bottom: 16px;">
+          <el-input class="width200" />
+        </el-form-item>
+        <el-form-item :label="$t('acquisitionNodeState.nodeIp') + '：'" style="margin-bottom: 16px;">
+          <el-input class="width200" />
+        </el-form-item>
+        <el-form-item :label="$t('acquisitionNodeState.nodePort') + '：'" style="margin-bottom: 16px;">
+          <el-input class="width200'" />
+        </el-form-item>
+        <br>
+        <el-form-item :label="$t('acquisitionNodeState.groupName') + '：'">
+          <el-select class="width200">
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item :label="$t('acquisitionNodeState.nodeType') + '：'">
+          <el-select class="width200">
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item :label="$t('acquisitionNodeState.status') + '：'">
+          <el-select class="width200">
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="queryBtn">{{$t('public.query')}}</el-button>
+        </el-form-item>
+      </el-form>
     </div>
-    <!--表格-->
-    <el-table :data="tableData" border>
-      <el-table-column type="selection" fixed />
-      <el-table-column :label="$t('public.index')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.nodeName')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.ip')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.port')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.nodeGroup')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.nodeType')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.equipmentNumber')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.description')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.status')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.executionStatus')" header-align="center" align="center" />
-      <el-table-column :label="$t('acquisitionNodeState.lastExecutionTime')" header-align="center" align="center" />
-    </el-table>
-    <!--分页-->
-    <pages
-      :total='options.total'
-      :currentPage='options.currentPage'
-      :pageSize='options.pageSize'
-      :firstPage='options.firstPage'
-      :lastPage='options.lastPage'
-      @handleCurrentChangeSub="handleCurrentChange" />
+    <div class="padding20">
+      <!--全局操作-->
+      <div class="marBottom16 clearfix">
+        <el-button @click="runMonitoring">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.runMonitoring')}}
+        </el-button>
+        <el-button @click="stopMonitoring">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.stopMonitoring')}}
+        </el-button>
+        <el-button @click="banMonitoring">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.banMonitoring')}}
+        </el-button>
+        <el-button @click="temporarilyIgnore">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.temporarilyIgnore')}}
+        </el-button>
+        <el-button @click="cancelIgnore">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.cancelIgnore')}}
+        </el-button>
+        <el-button @click="offLine">
+          <i class="el-icon-circle-close-outline"></i>
+          {{$t('acquisitionNodeState.offLine')}}
+        </el-button>
+        <el-select class="fr width200">
+          <el-option></el-option>
+        </el-select>
+      </div>
+      <!--表格-->
+      <el-table :data="tableData" stripe>
+        <el-table-column type="selection" fixed />
+        <el-table-column :label="$t('public.index')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.nodeName')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.ip')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.port')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.nodeGroup')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.nodeType')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.equipmentNumber')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.description')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.status')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.executionStatus')" header-align="center" align="center" />
+        <el-table-column :label="$t('acquisitionNodeState.lastExecutionTime')" header-align="center" align="center" />
+      </el-table>
+      <!--分页-->
+      <pages
+        :total='options.total'
+        :currentPage='options.currentPage'
+        :pageSize='options.pageSize'
+        :firstPage='options.firstPage'
+        :lastPage='options.lastPage'
+        @handleCurrentChangeSub="handleCurrentChange" />
+    </div>
   </Box>
 </template>
 
