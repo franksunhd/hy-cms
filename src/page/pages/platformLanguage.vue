81e72e042d6c72304bfe33c0d7fe6dfd443fbe6c
franksunhd <sun_si_yan@163.com>
2019-03-26 09:29:15 +0800

    平台语言设置按钮禁用逻辑

--- src/page/pages/platformLanguage.vue
+++ src/page/pages/platformLanguage.vue
@@ -50,33 +50,38 @@
           <i class="el-icon-circle-close-outline"></i>
           {{$t('public.disable')}}
         </el-button>
-        <el-button class="queryBtn" :disabled="disableBtn.more" @click="deleteData">
+        <el-button class="queryBtn" :disabled="disableBtn.edit" @click="deleteData">
           <i class="el-icon-delete"></i>
           {{$t('public.delete')}}
         </el-button>
-        <el-button @click="setDefault" :disabled="disableBtn.more">
+        <el-button @click="setDefault" :disabled="disableBtn.edit">
           <i class="el-icon-delete"></i>
           {{$t('platformLanguage.setDefault')}}
         </el-button>
-        <el-button @click="importFunction" :disabled="disableBtn.more">
+        <el-button @click="importFunction" :disabled="disableBtn.edit">
           <i class="el-icon-delete"></i>
           {{$t('platformLanguage.importFunction')}}
         </el-button>
-        <el-button @click="importData" :disabled="disableBtn.more">
+        <el-button @click="importData" :disabled="disableBtn.edit">
           <i class="el-icon-delete"></i>
           {{$t('platformLanguage.importData')}}
         </el-button>
       </div>
       <!--表格-->
-      <el-table :data="tableData" border>
-        <el-table-column type="selection" fixed />
+      <el-table :data="tableData" stripe @select="selectTableNum" @select-all="selectTableNum">
+        <el-table-column type="selection" fixed header-align="center" align="center"/>
         <el-table-column :label="$t('public.index')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.languageCodes')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.languageName')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.description')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.sort')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.isDefault')" header-align="center" align="center" />
-        <el-table-column :label="$t('platformLanguage.status')" header-align="center" align="center" />
+        <el-table-column :label="$t('platformLanguage.status')" header-align="center" align="center">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status === 1">启用</span>
+            <span v-if="scope.row.status === 0" style="color: red;">禁用</span>
+          </template>
+        </el-table-column>
         <el-table-column :label="$t('platformLanguage.createName')" header-align="center" align="center" />
         <el-table-column :label="$t('platformLanguage.createTime')" header-align="center" align="center" />
       </el-table>
@@ -155,7 +160,7 @@
         status:'',
         dialogVisible:false,
         tableData:[
-          {},{}
+          {status:1},{status:0},{status:1},{status:0},{status:1},{status:1}
         ],
         options:{
           total:1000, // 总条数
@@ -167,6 +172,51 @@
       }
     },
     methods: {
+      // 当前选中条数
+      selectTableNum(data){
+        var _t = this;
+        switch (data.length) {
+          case 0: // 未选中
+            _t.disableBtn.disable = true;
+            _t.disableBtn.edit = true;
+            _t.disableBtn.enable = true;
+            _t.disableBtn.more = true;
+            break;
+          case 1: // 单选
+            _t.disableBtn.edit = false;
+            _t.disableBtn.more = false;
+            data.forEach(function (item) {
+              if (item.status === 0) {
+                _t.disableBtn.enable = false;
+              } else if (item.status === 1) {
+                _t.disableBtn.disable = false;
+              }
+            });
+            break;
+          default: // 多选
+            _t.disableBtn.edit = true;
+            _t.disableBtn.more = false;
+            var disableFlag = 0, enableFlag = 0;
+            for (var i = 0;i < data.length;i++){
+              if (data[i].status === 0) {
+                disableFlag++;
+              } else if (data[i].status === 1) {
+                enableFlag++;
+              }
+            }
+            if (disableFlag > 0 && enableFlag > 0) {
+              _t.disableBtn.enable = true;
+              _t.disableBtn.disable = true;
+            } else if (disableFlag === 0 && enableFlag > 0) {
+              _t.disableBtn.enable = true;
+              _t.disableBtn.disable = false;
+            } else if (disableFlag > 0 && enableFlag === 0) {
+              _t.disableBtn.enable = false;
+              _t.disableBtn.disable = true;
+            }
+            break;
+        }
+      },
       // 改变当前页码
       handleCurrentChange(val){
         console.log(val)
