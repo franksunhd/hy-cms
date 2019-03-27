e8d0e9ba653a833b4aad7b62b129a9a94ecc06fb
franksunhd <sun_si_yan@163.com>
2019-03-27 10:56:53 +0800

    新建批量编辑导航页面

---
+++ src/page/pages/batchEditMemu.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "batchEditMemu",
+    data() {
+      return {}
+    },
+    methods: {},
+    created() {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
