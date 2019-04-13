7c350ddf74440cb9430bcd644337b20ddaccd627
franksunhd <sun_si_yan@163.com>
2019-04-13 16:14:42 +0800

    许可证信息维护样式，还差弹出层样式

--- src/page/pages/licenseInformation.vue
+++ src/page/pages/licenseInformation.vue
@@ -22,32 +22,33 @@
         <el-form-item :label="$t('license.effectiveModule') + '：'">ALL</el-form-item>
       </el-form>
     </div>
-    <div class="padding20 borderBottom">
+    <div class="padding20 borderBottom licensenInfo-btn-box">
       <el-button size="small" @click="dialogVisible = true">{{$t('license.uploadLicense')}}</el-button>
-      <el-button size="small" @click="isShow = true">{{$t('license.viewHistory')}}</el-button>
+      <el-button type="primary" size="small" @click="isShow = true">{{$t('license.viewHistory')}}</el-button>
     </div>
     <!--更新许可证书-->
     <el-dialog
       append-to-body
+      class="licenseInformation-dialog"
       :title="$t('header.head')"
       :visible.sync="dialogVisible">
       <el-upload action="">
         <el-button size="small" type="primary">{{$t('public.clickUpload')}}</el-button>
       </el-upload>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false" size="small">{{$t('public.confirm')}}</el-button>
-        <el-button type="primary" @click="dialogVisible = false" size="small">{{$t('public.close')}}</el-button>
+        <el-button type="primary" class="alertBtn" @click="dialogVisible = false">{{$t('public.confirm')}}</el-button>
+        <el-button class="alertBtn" @click="dialogVisible = false">{{$t('public.close')}}</el-button>
       </span>
     </el-dialog>
     <!--查看证书历史-->
     <div class="padding20">
-      <el-table :data="tableData" v-if="isShow" stripe>
-        <el-table-column :label="$t('license.indexNumber')" type="index" header-align="center" align="center" />
-        <el-table-column :label="$t('license.customerName')" header-align="center" align="center" />
-        <el-table-column :label="$t('license.allNumber')" header-align="center" align="center" />
-        <el-table-column :label="$t('license.validityDate')" header-align="center" align="center"/>
-        <el-table-column :label="$t('license.authorizationModule')" header-align="center" align="center" />
-        <el-table-column :label="$t('license.authorizationUpdateTime')" header-align="center" align="center" />
+      <el-table :data="tableData" v-if="isShow" stripe class="indexTableBox">
+        <el-table-column width="90px" :label="$t('license.indexNumber')" type="index" header-align="left" align="left" />
+        <el-table-column prop="1" min-width="170px" :label="$t('license.customerName')" header-align="left" align="left" />
+        <el-table-column prop="2" min-width="120px" :label="$t('license.allNumber')" header-align="left" align="left" />
+        <el-table-column prop="3" min-width="190px" :label="$t('license.validityDate')" header-align="left" align="left"/>
+        <el-table-column prop="4" min-width="140px" :label="$t('license.authorizationModule')" header-align="left" align="left" />
+        <el-table-column prop="5" min-width="160px" :label="$t('license.authorizationUpdateTime')" header-align="left" align="left" />
       </el-table>
     </div>
   </Box>
@@ -62,7 +63,16 @@
       return {
         dialogVisible:false,
         isShow:false,
-        tableData:[{},{}]
+        tableData:[
+          {
+            1:'你从为别玩碧波被窝我饿哦北欧文本你从为别玩碧波被窝对我饿哦北欧文本',
+            2:'99999',
+            3:'2019-99-99至9999-99-99',
+            4:'我饿哦北欧文本你从为别玩碧波被窝对',
+            5:''
+          },
+          {}
+        ]
       }
     },
     methods:{
@@ -74,6 +84,16 @@
   }
 </script>
 
-<style scoped>
+<style>
+  .licenseInformation-dialog .el-dialog {
+    width: 640px;
+    height: 434px;
+  }
 
+  .licenseInformation-dialog .el-dialog__footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 </style>
