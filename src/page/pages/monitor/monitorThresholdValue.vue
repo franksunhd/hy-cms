937a98a5e6f90be7ea32208b266a606be1632c67
franksunhd <sun_si_yan@163.com>
2019-04-26 14:10:38 +0800

    新增监测阈值组件

--- src/page/pages/monitor/monitorThresholdValue.vue
+++ src/page/pages/monitor/monitorThresholdValue.vue
@@ -3,9 +3,16 @@
     <!--面包屑区域-->
     <div class="Breadcrumb">
       <el-breadcrumb>
-        <el-breadcrumb-item>监测管理</el-breadcrumb-item>
-        <el-breadcrumb-item>监测汇总</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.monitorManage')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.equipmentMonitoring')}}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$t('breadcrumb.monitorThreshold')}}</el-breadcrumb-item>
       </el-breadcrumb>
+    </div>
+    <!--返回上一级-->
+    <div class="padding20" style="padding-bottom: 10px;">
+      <div class="grayBg">
+        111
+      </div>
     </div>
   </Box>
 </template>
